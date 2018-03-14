5d960ed8c528bc70b7aa486fa258897005daeeb2
mls912 <mstring12@gmail.com>
2018-03-13 18:28:48 -0600

    heroku fix

---
+++ app/data/friends.js
@@ -0,0 +1,90 @@
+// Data structure that will store the friends data
+var friends = [
+  {
+      name: "Matt",
+      photo: "./public/assets/matt.jpg",
+      scores: [
+      "3",
+      "4",
+      "4",
+      "3",
+      "4",
+      "2",
+      "4",
+      "2",
+      "4",
+      "3"
+      ]
+  },
+
+  {
+      name: "Amy",
+      photo: "./public/assets/amy.jpg",
+      scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "3",
+      "2",
+      "2",
+      "4",
+      "4",
+      "2"
+      ]
+  },
+
+  {
+      name: "Olivia",
+      photo: "./public/assets/olivia.jpg",
+      scores: [
+      "5",
+      "2",
+      "5",
+      "2",
+      "3",
+      "2",
+      "3",
+      "1",
+      "2",
+      "5"
+      ]
+  },
+
+  {
+      name: "Steve",
+      photo: "./public/assets/steve.jpg",
+      scores: [
+      "4",
+      "4",
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "2",
+      "5"
+      ]
+  },
+
+  {
+      name: "Aaron",
+      photo: "https://clutchpoints.com/wp-content/uploads/2017/12/Aaron-Rodgers-2.png",
+      scores: [
+      "4",
+      "2",
+      "1",
+      "5",
+      "3",
+      "2",
+      "5",
+      "2",
+      "2",
+      "5"
+      ]
+  }
+
+];
+
+module.exports = friends;
